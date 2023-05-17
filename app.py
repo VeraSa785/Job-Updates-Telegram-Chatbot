b1d18e76638b52872cb153c77fccfdd2dece0502
verasazonova <vera.zhokhova@gmail.com>
2023-05-17 15:11:35 -0700

    Added website updates logic with check_website function

--- app.py
+++ app.py
@@ -1,22 +1,81 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+import logging
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import requests
 
 load_dotenv()
 CHAT_BOT_TOKEN = os.getenv("CHAT_BOT_TOKEN")
-WEBSITE = os.getenv("WEBSITE")
 
-from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
+# Set up logging
+logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=CHAT_BOT_TOKEN)
-dp = Dispatcher(bot)
+# Initialize the bot and dispatcher
+bot_token = CHAT_BOT_TOKEN
+bot = Bot(token=bot_token)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
 
-@dp.message_handler()
-async def get_message(message: types.Message):
+# URL to monitor
+url = "https://www.smartsheet.com/careers-list?location=Bellevue%2C+WA%2C+USA&department=Engineering+-+Developers&position="
+
+# Dictionary to store chat IDs and their associated URLs
+chat_ids_urls = {}
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    # Get the chat ID
     chat_id = message.chat.id
-    text = "Hi Vera!"
 
-    sent_message = await bot.send_message(chat_id=chat_id, text=text)
-    print(sent_message.to_python())
+    # Add the chat ID to the dictionary with the URL to monitor
+    chat_ids_urls[chat_id] = url
 
-executor.start_polling(dp)
+    await message.reply("Monitoring has started!")
+
+
+async def check_website():
+    while True:
+        try:
+            for chat_id, url in chat_ids_urls.items():
+                # Fetch the HTML content of the web page
+                response = requests.get(url)
+                response.raise_for_status()
+                html_content = response.text
+
+                # Get the previous HTML content for the chat ID
+                previous_html = chat_ids_urls.get(chat_id)
+
+                # Check for changes in the HTML content
+                if previous_html is not None and html_content != previous_html:
+                    # Changes detected, send a message to the chat ID
+                    await bot.send_message(chat_id= chat_id, text="Website has been updated!")
+
+                else:
+                    await bot.send_message(chat_id= chat_id, text="No updates")
+
+                # Update the HTML content for the chat ID
+                chat_ids_urls[chat_id] = html_content
+
+                await asyncio.sleep(3600)
+
+        except Exception as e:
+            logging.error(f"An error occurred while checking the website: {str(e)}")
+
+            await asyncio.sleep(300)
+
+
+async def start_bot():
+    try:
+        await dp.start_polling()
+        asyncio.create_task(check_website())
+        await asyncio.Event().wait()
+    finally:
+        await dp.storage.close()
+        await dp.storage.wait_closed()
+        await bot.session.close()
+
+
+if __name__ == '__main__':
+    asyncio.run(start_bot())
